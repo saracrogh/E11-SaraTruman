815cd8bdc84434c72778252712d7b630314a01a1
TrumanOB <124406933+TrumanOB@users.noreply.github.com>
2023-02-10 15:32:33 -0800

    Update air_quality.py

--- air_quality.py
+++ air_quality.py
@@ -1,6 +1,8 @@
 import time
 import board
 import busio
+import adafruit_pm25
+import csv
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_pm25.i2c import PM25_I2C
 
@@ -10,6 +12,10 @@ reset_pin = None
 import serial
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 
+# Connect to a PM2.5 sensor over UART
+from adafruit_pm25.uart import PM25_UART
+pm25 = PM25_UART(uart, reset_pin)
+
 # Create library object, use 'slow' 100KHz frequency!
 #i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 # Connect to a PM2.5 sensor over I2C
@@ -17,12 +23,24 @@ uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 
 print("Found PM2.5 sensor, reading data...")
 
-while True:
-    time.sleep(1)
+#CSV STUFF:
+#f = open("Lab4Data.csv","w", newline='')
 
+#meta_data = ["Time", ]
+#writer = csv.writer(f)
+#writer.writerow(meta_data)
+#writer.writerow(data) #loop this line to add data to file
+#f.close()
+
+
+for i in range(1,10):
+    time.sleep(1)
+    timeCurrent = time.time()
+    print(timeCurrent)
+    
     try:
         aqdata = pm25.read()
-        # print(aqdata)
+        print(aqdata)
     except RuntimeError:
         print("Unable to read from sensor, retrying...")
         continue
@@ -48,3 +66,6 @@ while True:
     print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
     print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
     print("---------------------------------------")
+
+    
+    
