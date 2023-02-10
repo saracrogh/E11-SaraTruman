ee8e9bcc5eb4d9d3db757f447a44ff528854ade5
TrumanOB <124406933+TrumanOB@users.noreply.github.com>
2023-02-10 15:51:09 -0800

    Update air_quality.py

--- air_quality.py
+++ air_quality.py
@@ -16,21 +16,14 @@ uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 from adafruit_pm25.uart import PM25_UART
 pm25 = PM25_UART(uart, reset_pin)
 
-# Create library object, use 'slow' 100KHz frequency!
-#i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
-# Connect to a PM2.5 sensor over I2C
-#pm25 = PM25_I2C(i2c, reset_pin)
+
 
 print("Found PM2.5 sensor, reading data...")
 
-#CSV STUFF:
 f = open("Lab4Data.csv","w", newline='')
-
 meta_data = ["Time", "pm10_standard", "pm25_standard", "pm100_standard"]
 writer = csv.writer(f)
 writer.writerow(meta_data)
-
-
 
 for i in range(1,10):
     time.sleep(1)
@@ -45,29 +38,5 @@ for i in range(1,10):
         continue
     
     writer.writerow([timeCurrent, aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"]]) 
-    #loop this line to add data to file
-    
-    print()
-    print("Concentration Units (standard)")
-    print("---------------------------------------")
-    print(
-        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
-    )
-    print("Concentration Units (environmental)")
-    print("---------------------------------------")
-    print(
-        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
-    )
-    print("---------------------------------------")
-    print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
-    print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
-    print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
-    print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
-    print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
-    print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
-    print("---------------------------------------")
 
-      
 f.close()
