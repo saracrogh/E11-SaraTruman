a9b5f0dd53f22f853e2e9cbade79a0c0ef857324
saracrogh <124406892+saracrogh@users.noreply.github.com>
2023-02-17 14:44:16 -0800

    Create friday-example.py

---
+++ friday-example.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import time
+import random
+
+
+# In[ ]:
+
+
+while True:
+    itime= int(time.time())
+    value = random.random()
+    time.sleep(1)
+
+
+# In[ ]:
+
+
+
+
