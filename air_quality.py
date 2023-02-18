4486ef908969f1c4fed1554a1738c5461137b1d9
saracrogh <124406892+saracrogh@users.noreply.github.com>
2023-02-17 16:31:40 -0800

    Update air_quality.py

--- air_quality.py
+++ air_quality.py
@@ -17,12 +17,17 @@ uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 from adafruit_pm25.uart import PM25_UART
 pm25 = PM25_UART(uart, reset_pin)
 
+i2c = board.I2C()
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
+bme680.sea_level_pressure = 1013.25
+temperature_offset = -5
+timeElapsed = 0
 
 
 print("Found PM2.5 sensor, reading data...")
 
 f = open("Lab4Data.csv","w", newline='')
-meta_data = ["Time", "pm10_standard", "pm25_standard", "pm100_standard", "Gas" ]
+meta_data = ["Time", "pm10_standard", "pm25_standard", "pm100_standard"]
 writer = csv.writer(f)
 writer.writerow(meta_data)
 
@@ -38,6 +43,6 @@ for i in range(0,10):
         print("Unable to read from sensor, retrying...")
         continue
     
-    writer.writerow([timeCurrent, aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"]bme680.gas]) 
+    writer.writerow([timeCurrent, aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"],]) 
 
 f.close()
