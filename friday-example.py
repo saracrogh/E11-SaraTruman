6dcf60e785286063950144aa0037b5a8e5ad032c
saracrogh <124406892+saracrogh@users.noreply.github.com>
2023-02-17 16:45:45 -0800

    Update friday-example.py

--- friday-example.py
+++ friday-example.py
@@ -9,15 +9,22 @@ import adafruit_pm25
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_pm25.i2c import PM25_I2C
 import adafruit_bme680
+import board
 
 print (sys.argv)
 start_time = int(time.time())
 itime= start_time
 run_time= int(sys.argv[1])
-file_name = 'data.csv'
 
+file_name = 'data.csv'
+if(len(sys.argv)>2):
+    file_name=sys.argv[2]
+    
 print(file_name)
 file=open(file_name, "w", newline='')
+writer = csv.writer(file)
+meta_data= ["Time", "pm10_standard", "pm25_standard", "pm100_standard", "Temperature", "Gas", "Humidity","Pressure","Altitude"]
+writer.writerow(meta_data)
 
 reset_pin = None
 
@@ -30,23 +37,31 @@ from adafruit_pm25.uart import PM25_UART
 pm25 = PM25_UART(uart, reset_pin)
 
 i2c = board.I2C()
-#bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
-#bme680.sea_level_pressure = 1013.25
-#temperature_offset = -5
-timeElapsed = 0
-
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
+bme680.sea_level_pressure = 1013.25
 
 print("Found PM2.5 sensor, reading data...")
 
-f = open("Lab4Data.csv","w", newline='')
-meta_data = ["Time", "pm10_standard", "pm25_standard", "pm100_standard", "Temperature", "Gas", "Humidity","Pressure","Altitude"]
-writer = csv.writer(f)
-writer.writerow(meta_data)
 
 while itime < (start_time+run_time):
     itime = int (time.time())
     value = random.random()
     print(itime,value)
+    
+    time.sleep(1)
+    timeCurrent = time.time()
+    print(timeCurrent)
+    
+    try:
+        aqdata = pm25.read()
+        print(aqdata)
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
+        continue
+    
+    writer.writerow([timeCurrent, aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"],]) 
+
+f.close()
     time.sleep(1)
     try:
         aqdata = pm25.read()
@@ -55,7 +70,7 @@ while itime < (start_time+run_time):
         print("Unable to read from sensor, retrying...")
         continue
     
-    writer.writerow([itime, aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"],(bme680.temperature + temperature_offset),bme680.gas,bme680.relative_humidity,bme680.pressure,bme680.altitude]) 
+    writer.writerow([itime, aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"],bme680.temperature,bme680.gas,bme680.relative_humidity,bme680.pressure,bme680.altitude]) 
     
 
 f.close()
