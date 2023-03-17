e5c8000df1d6e867134bb0b6e6c94de8471eca79
TrumanOB <124406933+TrumanOB@users.noreply.github.com>
2023-03-17 15:28:14 -0700

    Update lab_9.py

--- lab_9.py
+++ lab_9.py
@@ -3,6 +3,18 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
+
+def my_callback(channel):
+    if GPIO.input(channel) == GPIO.HIGH:
+        print('\n▼  at ' + str(datetime.datetime.now()))
+    else:
+        print('\n ▲ at ' + str(datetime.datetime.now()))
+          
+GPIO.setmode(GPIO.BCM)
+PIO.setup(6, GPIO.IN)
+#PIO.add_event_detect(6, GPIO.BOTH, callback=my_callback)
+
+
 channel = 2
 start_time = int(time.time())
 i = 0
