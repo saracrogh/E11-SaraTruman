09ea6e6da0c09d8ed420b308f0278fea0c5dc848
TrumanOB <124406933+TrumanOB@users.noreply.github.com>
2023-03-17 15:21:48 -0700

    Update lab_9.py

--- lab_9.py
+++ lab_9.py
@@ -2,9 +2,10 @@
  
 import RPi.GPIO as GPIO
 import datetime
+import time
 channel = 2
 start_time = int(time.time())
 
-while true:
+while True:
  GPIO.wait_for_edge(channel, GPIO.FALLING)
  print(int(time.time())
