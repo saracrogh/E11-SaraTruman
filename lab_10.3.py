732a86ddc945cd37eb7a117de610d16af2c17a41
TrumanOB <124406933+TrumanOB@users.noreply.github.com>
2023-03-24 15:50:41 -0700

    Create lab_10.3.py

---
+++ lab_10.3.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import datetime
+import time
+
+channel = 6
+
+def my_callback(channel):
+ print('\falling at ' + str(datetime.datetime.now()))
+  
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(channel, GPIO.FALLING, callback=my_callback, bouncetime=10)
+GPIO.setwarnings(False)
+ 
+while True:
+ print("Running while loop")
+ time.sleep(15)
+  
+print("Goodbye!")
