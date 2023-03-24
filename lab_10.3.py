e6948a6dc59628cb6d4977a7b33f445511558bc5
TrumanOB <124406933+TrumanOB@users.noreply.github.com>
2023-03-24 16:01:23 -0700

    Update lab_10.3.py

--- lab_10.3.py
+++ lab_10.3.py
@@ -1,19 +1,47 @@
 import RPi.GPIO as GPIO
 import datetime
 import time
+import sys
+import csv
 
-channel = 6
-
+if len(sys.argv) > 1:
+ run_time = sys.argv[1]
+ if len(sys.argv) > 2:
+  count_time = sys.argv[2]
+  if len(sys.argv) > 3:
+   output_file_name = sys.argv[3]
+   output_file_name = output_file_name + ".csv"
+ 
 def my_callback(channel):
  print('\falling at ' + str(datetime.datetime.now()))
+ counts = counts + 1
   
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(channel, GPIO.FALLING, callback=my_callback, bouncetime=10)
 GPIO.setwarnings(False)
  
-while True:
- print("Running while loop")
- time.sleep(15)
-  
-print("Goodbye!")
+channel = 6
+
+f = open(output_file_name,"w")
+meta_data = ["CPM","Time Tags"]
+
+f = open(output_file_name,"w",newline='')
+writer = csv.writer(f)
+writer.writerow(meta_data)
+
+startTime=int(time.time())
+iTime=startTime
+counts = 0
+list_of_times = []
+originalStart = int(time.time())
+
+
+while iTime < (startTime + run_time):
+ time.sleep(count_time)
+ print("Counts in the last minute: " + str(counts))
+ data = [str(counts),str(time.time())]
+ writer.writerow(data)
+ iTime = int(time.time())
+ 
+f.close()
