5c7c9c52d04026423db38c1d4be8d5d15e2b5bb6
TrumanOB <124406933+TrumanOB@users.noreply.github.com>
2023-03-24 15:16:23 -0700

    Create lab_10.2.py

---
+++ lab_10.2.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+ 
+import RPi.GPIO as GPIO
+import datetime
+import time
+
+channel = 17
+  
+  
+def my_callback(channel):
+      print('\n▼  at ' + str(datetime.datetime.now()))
+ 
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(channel, GPIO.IN)
+    GPIO.add_event_detect(channel, GPIO.FALLING, callback=my_callback)
+ 
+    message = raw_input('\nPress any key to exit.\n')
+ 
+finally:
+    GPIO.cleanup()
+ 
+while True:
+  time.sleep(10)
+  
+print("Goodbye!")
