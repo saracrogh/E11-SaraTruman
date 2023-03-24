26c76f06c5b2672395ea13d1bc1dff89d514a581
TrumanOB <124406933+TrumanOB@users.noreply.github.com>
2023-03-24 14:54:57 -0700

    Update lab_9.py

--- lab_9.py
+++ lab_9.py
@@ -5,31 +5,27 @@ import datetime
 import time
 
 
-channel = 17
-tests = 4
-time_per_test = 30
-
-
-
+channel = 2
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
 i = 0
-while i < tests:
+while i < 5:
  i = i + 1
  start_time = int(time.time())
- end_time = start_time + time_per_test
+ end_time = start_time + 10
  counts = 0
 
  while time.time() <= end_time:
   GPIO.wait_for_edge(channel, GPIO.FALLING, timeout = 5)
   if False == GPIO.input(channel):
    counts = counts + 1
-   print(str(datetime.datetime.now()))
+   # print(str(datetime.datetime.now()))
   time.sleep(0.005)
  
  print(counts)
  
 
+ 
  
  
