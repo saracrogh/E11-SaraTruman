fe19637b627d53bf6750ab48b9bfb0c5322e2a4e
TrumanOB <124406933+TrumanOB@users.noreply.github.com>
2023-03-24 15:28:56 -0700

    Update lab_10.2.py

--- lab_10.2.py
+++ lab_10.2.py
@@ -11,7 +11,7 @@ def my_callback(channel):
   
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(channel, GPIO.FALLING, callback=my_callback)
+GPIO.add_event_detect(channel, GPIO.FALLING, callback=my_callback, bouncetime=1)
  
 while True:
  print("Running while loop")
